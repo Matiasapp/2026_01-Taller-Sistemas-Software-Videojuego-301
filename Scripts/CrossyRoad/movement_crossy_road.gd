c5e2c9db17e65afab20ca0f0bd84be2d567b78e6
Matias Ponce <mponcep@utem.cl>
2026-06-03 17:09:54 -0400

    cambios finales y minijuego implementado, falta conectar con el flujo principal

--- Scripts/CrossyRoad/movement_crossy_road.gd
+++ Scripts/CrossyRoad/movement_crossy_road.gd
@@ -1,11 +1,27 @@
 extends Node2D 
 
+@onready var valor_por_casilla: int = 2
 @export var tamaño_casilla = 64
 @export var velocidad_movimiento = 0.15 
+@export var limite_retroceso: int = 3 
+@onready var puntaje = $"../Hud/Puntaje"
+@onready var label_tiempo_restante = $"../Hud/TiempoRestante"
+var tiempo_restante: float = 60.0
+@onready var label_dinero_obtenido = $"../Hud/Resumen/PanelFinal/DineroObtenido"
+@onready var dinero_obtenido: int = 0
+@onready var panel_tutorial = $"../Hud/Tutorial"
+var juego_iniciado: bool = false
+@export var label_parpadeo: Label
+@export var pantalla_final: Control
+@export var label_puntaje_final: Label
+@onready var atropellado: bool = false
+
 @onready var anim = $AnimatedSprite2D
 @onready var genero = DATOSGLOBALES.genero_jugador
 @onready var entorno_visual = $WorldEnvironment.environment
 @onready var raycast = $RayCast2D
+@onready var camara = $Camera2D 
+
 
 var frames_hombre = preload("res://Assets/Sprites/animaciones_hombre.tres")
 var frames_mujer = preload("res://Assets/Sprites/animaciones_mujer.tres")
@@ -16,35 +32,94 @@ var ultima_direccion = Vector2(0, 1)
 var posicion_logica: Vector2 
 var tween_actual: Tween 
 
+var posicion_inicial_y: float
+var maximas_casillas_avanzadas: int = 0
+
 func _ready() -> void:
+	if panel_tutorial:
+		panel_tutorial.show()
+		
+		# Efecto de parpadeo (Blink) infinito
+		if label_parpadeo:
+			var tween_blink = create_tween().set_loops() 
+			tween_blink.tween_property(label_parpadeo, "modulate:a", 0.0, 0.6) 
+			tween_blink.tween_property(label_parpadeo, "modulate:a", 1.0, 0.6)
+			
 	z_index = 2
 	entorno_visual.adjustment_saturation = 1.0
 	Engine.time_scale = 1.0
+	
 	if genero == "Masculino":
 		anim.sprite_frames = frames_hombre
 	else:
 		anim.sprite_frames = frames_mujer
 		
 	posicion_logica = position
+	posicion_inicial_y = position.y 
 	actualizar_idle()
 	
-	# Conexión por código: Conecta la señal del hijo Area2D a la función de muerte
-	# (Asegúrate de que tu nodo hijo Area2D se llame exactamente "Hitbox")
+	if camara:
+		camara.top_level = true
+		camara.global_position = global_position
+		
+	# Sincronizar UI al iniciar
+	if puntaje: puntaje.text = str(maximas_casillas_avanzadas)
+	if pantalla_final: pantalla_final.hide()
+
+func _process(delta: float) -> void:
+	if camara and not esta_muerto:
+		# En el eje X, la cámara te sigue a los lados libremente
+		var destino_x = global_position.x
+		
+		# En el eje Y, la cámara SOLO sube basada en tu récord máximo de casillas
+		var destino_y = posicion_inicial_y - (maximas_casillas_avanzadas * tamaño_casilla)
+		
+		# Movemos la cámara suavemente hacia ese destino usando lerp
+		var posicion_destino = Vector2(destino_x, destino_y)
+		camara.global_position = camara.global_position.lerp(posicion_destino, 8.0 * delta)
+		
+		if juego_iniciado and tiempo_restante > 0:
+			tiempo_restante -= delta # Restamos el tiempo que pasó en este frame
+			
+			if label_tiempo_restante:
+				label_tiempo_restante.text = "Tiempo Restante: " + str(int((ceil(tiempo_restante)))) + "s"
+				
+			# Si el tiempo llega a cero o menos
+			if tiempo_restante <= 0:
+				tiempo_restante = 0
+				if label_tiempo_restante:
+					label_tiempo_restante.text = "Tiempo Restante: 0s"
+				
+				print("¡Se acabó el tiempo!")
+				morir()
 
 func _unhandled_input(event: InputEvent) -> void:
-	if se_esta_moviendo:
-		return
-	if esta_muerto:
+	if se_esta_moviendo or esta_muerto:
 		return
 		
+	if not juego_iniciado:
+		if event.is_action_pressed("mover_arriba") or event.is_action_pressed("mover_abajo") or event.is_action_pressed("mover_izquierda") or event.is_action_pressed("mover_derecha"):
+			juego_iniciado = true
+			if panel_tutorial:
+				panel_tutorial.hide()
+
 	var direccion = Vector2.ZERO
+	var casillas_actuales = int((posicion_inicial_y - posicion_logica.y) / tamaño_casilla)
 	
 	if event.is_action_pressed("mover_arriba"):
 		direccion = Vector2.UP
 		anim.play("saltar_arriba")
+			
 	elif event.is_action_pressed("mover_abajo"):
-		direccion = Vector2.DOWN
-		anim.play("saltar_abajo")
+		var casilla_destino = casillas_actuales - 1
+		var casilla_minima_permitida = maximas_casillas_avanzadas - limite_retroceso
+		
+		if casilla_destino >= casilla_minima_permitida:
+			direccion = Vector2.DOWN
+			anim.play("saltar_abajo")
+		else:
+			print("¡Límite de retroceso! No puedes volver más de ", limite_retroceso, " casillas.")
+			
 	elif event.is_action_pressed("mover_izquierda"):
 		direccion = Vector2.LEFT
 		anim.play("saltar_izquierda")
@@ -55,21 +130,18 @@ func _unhandled_input(event: InputEvent) -> void:
 	if direccion != Vector2.ZERO:
 		ultima_direccion = direccion
 		
-		# --- NUEVO: SISTEMA DE COLISIÓN CON ÁRBOLES ---
-		
-		# 1. Apuntamos el láser hacia la casilla a la que queremos ir
-		raycast.target_position = direccion * (tamaño_casilla-50)
-		
-		# 2. Forzamos a Godot a actualizar la física del láser en este milisegundo
+		raycast.target_position = direccion * (tamaño_casilla - 33)
 		raycast.force_raycast_update()
 		
-		# 3. Preguntamos: ¿El láser tocó algo en la Capa 2 (un árbol)?
 		if raycast.is_colliding():
-			# Hay un árbol. No saltamos, pero actualizamos la animación idle 
-			# para que el personaje se gire y "mire" la pared/árbol.
 			actualizar_idle()
 		else:
-			# El camino está libre, procedemos a dar el salto
+			if direccion == Vector2.UP:
+				if (casillas_actuales + 1) > maximas_casillas_avanzadas:
+					maximas_casillas_avanzadas = casillas_actuales + 1
+					if puntaje:
+						puntaje.text = str(maximas_casillas_avanzadas)
+			
 			dar_salto(direccion)
 
 func dar_salto(direccion: Vector2) -> void:
@@ -93,31 +165,35 @@ func dar_salto(direccion: Vector2) -> void:
 # --- DETECCIÓN DE ATROPELLO ---
 
 func morir() -> void:
-	# Bloqueamos controles inmediatamente
+	if esta_muerto: return 
+	
 	esta_muerto = true
 	se_esta_moviendo = true 
 	anim.play("atropellado")
-	# Si nos atropellan a mitad de un salto, congelamos la posición visual
+	
 	if tween_actual and tween_actual.is_running():
 		tween_actual.kill()
-		
-	print("¡Game Over! Te atropellaron.")
 	
-	# ARREGLO 1: Forzamos que los ajustes de color estén encendidos
 	entorno_visual.adjustment_enabled = true
-	
-	# Activamos la cámara lenta para el juego
-	Engine.time_scale = 0.3
+	Engine.time_scale = 0.3 # Cámara lenta
 	
 	var tween_color = create_tween()
-	# ARREGLO 2: Le decimos al Tween que se anime en "tiempo real" ignorando el slow-motion
 	tween_color.set_ignore_time_scale(true)
 	tween_color.tween_property(entorno_visual, "adjustment_saturation", 0.0, 1.5)
 	
-	# ARREGLO 3: Le pasamos 'true' al final del Timer para que también ignore el slow-motion
-	# Parámetros: (tiempo, process_always, process_in_physics, ignore_time_scale)
-	get_tree().create_timer(1.5, true, false, true).timeout.connect(func(): get_tree().reload_current_scene())
-# --- FUNCIONES DE AYUDA ---
+	print("¡Game Over!")
+	calculo_dinero_final()
+	# Vamos directo a la pantalla final
+	get_tree().create_timer(1.5, true, false, true).timeout.connect(mostrar_pantalla_final)
+
+func mostrar_pantalla_final() -> void:
+	Engine.time_scale = 0.0 # Congelamos el juego
+	
+	if pantalla_final:
+		pantalla_final.show()
+	if label_puntaje_final:
+		label_dinero_obtenido.text = "Dinero Obtenido: $" + str(dinero_obtenido)
+		label_puntaje_final.text = "PUNTAJE MÁXIMO: " + str(maximas_casillas_avanzadas)
 
 func actualizar_idle() -> void:
 	if ultima_direccion.x > 0:
@@ -129,11 +205,14 @@ func actualizar_idle() -> void:
 	else:
 		anim.play("idle_abajo")
 
-
 func _on_area_2d_area_entered(area: Area2D) -> void:
-		# En el instante en que cualquier Area2D toque a nuestra Hitbox, 
-	# comprobamos si tiene la etiqueta "vehiculos"
 	if area.is_in_group("vehiculos"):
+		atropellado = true
 		z_index = 1
 		morir()
-	print("¡Algo me acaba de tocar! Se llama: ", area.name)
+		
+		
+func calculo_dinero_final() -> void:
+	dinero_obtenido = (maximas_casillas_avanzadas * valor_por_casilla)
+	if atropellado:
+		dinero_obtenido = dinero_obtenido - 50
--- Scripts/CrossyRoad/world_generator.gd
+++ Scripts/CrossyRoad/world_generator.gd
@@ -1,73 +1,80 @@
 extends Node2D
 
-# Lista donde pondremos nuestras franjas (Pasto y Calle)
 @export var franjas_posibles: Array[PackedScene]
-
-# Cuántas franjas queremos que se generen al iniciar el juego
-@export var franjas_iniciales: int = 64
 @export var tamaño_casilla: int = 64
+@export var franjas_iniciales: int = 25 # Con 30 para empezar es más que suficiente
+@export var buffer_generacion: int = 20 # Mantiene siempre 25 casillas por delante del jugador
 
-# Empezamos en Y = 0 y de ahí iremos subiendo hacia los números negativos
+# NUEVO: Necesitamos saber dónde está el jugador para generar el mapa frente a él
+@export var jugador: Node2D 
+
 var posicion_y_actual: int = 0 
+var franjas_activas: Array[Node] = [] # NUEVO: Lista para guardar y borrar las franjas viejas
 
 func _ready() -> void:
-	randomize() # Asegura que el azar sea diferente en cada partida
+	randomize() 
 	
-	# Opcional pero recomendado: Generar unas cuantas franjas de pasto seguidas 
-	# al principio para que el jugador nazca en una zona segura sin autos.
-	# Asumimos que tu pasto es el elemento 0 de la lista.
 	for i in range(3):
 		generar_franja_segura()
 		
-	# Generar el resto del mapa aleatoriamente
 	for i in range(franjas_iniciales - 3):
 		generar_franja_aleatoria()
 
+# NUEVO: Vigila constantemente al jugador
+func _process(_delta: float) -> void:
+	if not jugador:
+		return # Si no hay jugador asignado, no hacemos nada
+		
+	# Calculamos cuántas casillas hay entre el jugador y la última casilla generada
+	var distancia_y = jugador.position.y - posicion_y_actual
+	var casillas_de_distancia = int(distancia_y / tamaño_casilla)
+	
+	# Si el jugador se acerca al borde, generamos una nueva franja y borramos una vieja
+	if casillas_de_distancia < buffer_generacion:
+		generar_franja_aleatoria()
+		limpiar_franjas_viejas()
+
 func generar_franja_aleatoria() -> void:
-	# Nos aseguramos de tener ambas opciones configuradas en el Inspector
 	if franjas_posibles.size() < 2:
 		return
 		
 	var escena_elegida: PackedScene
-	
-	# Tiramos un dado virtual que da un número del 1 al 100
 	var probabilidad = randi_range(1, 100)
 	
-	# Si el número cae entre 1 y 20 (20% de probabilidad), generamos Pasto
 	if probabilidad <= 20:
 		escena_elegida = franjas_posibles[1] 
-	# Si cae entre 21 y 100 (80% de probabilidad), generamos Calle
 	else:
 		escena_elegida = franjas_posibles[0] 
 		
-	# Instanciamos la escena elegida por nuestro dado
-	var nueva_franja = escena_elegida.instantiate()
-	
+	instanciar_y_posicionar(escena_elegida)
+
+func generar_franja_segura() -> void:
+	if franjas_posibles.size() > 0:
+		instanciar_y_posicionar(franjas_posibles[0])
+
+# NUEVO: Función unificada para no repetir código al crear casillas
+func instanciar_y_posicionar(escena: PackedScene) -> void:
+	var nueva_franja = escena.instantiate()
 	nueva_franja.position.y = posicion_y_actual
 	nueva_franja.position.x = 0
 	
 	add_child(nueva_franja)
 	move_child(nueva_franja, 0)
 	
-	# Revisamos si la franja es más gorda (como tu pasto doble)
+	# Guardamos la franja en nuestra lista para vigilarla
+	franjas_activas.append(nueva_franja)
+	
 	var salto_casillas = 1 
 	if "alto_en_casillas" in nueva_franja:
 		salto_casillas = nueva_franja.alto_en_casillas
 		
 	posicion_y_actual -= (tamaño_casilla * salto_casillas)
 
-
-func generar_franja_segura() -> void:
-	if franjas_posibles.size() > 0:
-		var franja_pasto = franjas_posibles[0].instantiate()
-		franja_pasto.position.y = posicion_y_actual
-		franja_pasto.position.x = 0
-		add_child(franja_pasto)
-		move_child(franja_pasto, 0)
-		
-		# Hacemos la misma validación aquí para el inicio del juego
-		var salto_casillas = 1
-		if "alto_en_casillas" in franja_pasto:
-			salto_casillas = franja_pasto.alto_en_casillas
-			
-		posicion_y_actual -= (tamaño_casilla * salto_casillas)
+# NUEVO: Destructor de memoria
+func limpiar_franjas_viejas() -> void:
+	# Si hay más de 40 franjas en la pantalla, empezamos a borrar las que quedaron atrás
+	if franjas_activas.size() > 10:
+		# pop_front() saca el elemento más viejo de la lista (el que quedó hasta abajo)
+		var franja_vieja = franjas_activas.pop_front() 
+		if is_instance_valid(franja_vieja):
+			franja_vieja.queue_free() # Destruye el nodo de la escena liberando la memoria
