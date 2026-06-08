ca8060d44590d02aad187e2f505f59aa3031a4db
Matias Ponce <mponcep@utem.cl>
2026-06-08 19:58:19 -0400

    Revert "Merge pull request #13 from Matiasapp/Feature_soldadura"
    This reverts commit 879312b073a8820443f5f712c6c6c73444e4d7f7, reversing
    changes made to 9b5a457889b893814c8cb916fbd0a492d72eb20e.
    Motivo:nombre incorrecto del branch, se llamaba "Feature_soldadura" y el nombre correcto debería ser "feature/soldadura, además se autoaceptó el merge sin el visto bueno, por lo que se decidió revertir el merge y volver a hacer el merge con el nombre correcto del branch y con el visto bueno"

--- Scenes/Minigames/Soldadura/Script/Global.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-
-var piezas_completadas := 0
-
-var dinero := 0
--- Scenes/Minigames/Soldadura/Script/resultados.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-func _ready():
-
-	$LabelPiezas.text = "Piezas completadas: " + str(Global.piezas_completadas)
-
-	$LabelDinero.text = "Dinero ganado: $" + str(Global.dinero)
