bae553d9d029c7d48f57d929574fe021bf523554
cristopher923 <117414157+cristopher923@users.noreply.github.com>
2026-06-17 21:52:56 -0400

    feature-cliente-furioso
    Se crearon 3 clientes con diferentes diálogos, Se activa inicialmente la escena  bandera, al cargar al cliente. se debe pulsar el izquierdo para continuar (redirige al menú del juego)

---
+++ Scenes/Clientes/C_Enojado/Scripts/CargaClientefuriosos.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+var rng = RandomNumberGenerator.new()
+
+@onready var cliente_container = $ClienteContainer
+
+var clientes_enojados = [
+	preload("res://Scenes/Clientes/C_Enojado/Scenas/ClientesFuriosos/cliente_furioso_1.tscn"),
+	preload("res://Scenes/Clientes/C_Enojado/Scenas/ClientesFuriosos/cliente_furioso_2.tscn"),
+	preload("res://Scenes/Clientes/C_Enojado/Scenas/ClientesFuriosos/cliente_furioso_3.tscn"),
+]
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	rng.randomize()
+	var cliente = rng.randi_range(0,clientes_enojados.size()- 1)
+	var cliente_enojado = clientes_enojados[cliente].instantiate()
+	
+	add_child(cliente_enojado)
+
+
+func _on_color_rect_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		get_tree().change_scene_to_file("res://Scenes/Gameplay/GameScreen.tscn")
+	
+	
+
+
+func _on_continuar_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		get_tree().change_scene_to_file("res://Scenes/Gameplay/GameScreen.tscn")
